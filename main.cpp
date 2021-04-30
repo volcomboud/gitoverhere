f213badc456859541736c8429c367e377254ac76
volcomboud <83305133+volcomboud@users.noreply.github.com>
2021-04-30 10:07:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
+void calculer(int we, int x);
 int main(){
 int we = 2;
 int x=0;
 std::cout<<"c<est ca"<<std::endl;
+calculer(we,x);
 
     return 0;
+}
+
+void calculer(int we, int x){
+    int resultat = 0;
+    resultat=we*x;
+    
+    std::cout<<"Le resultat est "<<resultat<<std::endl;
+    
 }
